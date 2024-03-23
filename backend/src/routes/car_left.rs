70079ce6e0bc63ba913395a1cc26b6632985b412
kuba <kuba121201@gmail.com>
2024-03-23 01:13:16 +0100

    feat: add car left

---
+++ backend/src/routes/car_left.rs
@@ -0,0 +1,48 @@
+use crate::entities::car_log;
+use crate::entities::car_log::Entity as CarLog;
+use axum::{http::StatusCode, Extension, Json};
+use chrono::{FixedOffset, Utc};
+use sea_orm::{prelude::DateTimeWithTimeZone, ActiveModelTrait, DatabaseConnection, Set};
+use sea_orm::{ColumnTrait, EntityTrait, QueryFilter};
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize)]
+pub struct RequestCar {
+    spot_id: u8,
+}
+
+#[derive(Serialize)]
+pub struct ResponseTimeLeft {
+    car_left: DateTimeWithTimeZone,
+}
+
+pub async fn car_left(
+    Extension(database): Extension<DatabaseConnection>,
+    Json(body): Json<RequestCar>,
+) -> Result<Json<ResponseTimeLeft>, StatusCode> {
+    let car = CarLog::find()
+        .filter(car_log::Column::SpotId.eq(body.spot_id as i16))
+        .one(&database)
+        .await
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+
+    match car {
+        Some(car) => {
+            let mut car: car_log::ActiveModel = car.into();
+
+            let offset = FixedOffset::east_opt(1 * 60 * 60).unwrap();
+            let now_with_offset = Utc::now().with_timezone(&offset);
+            car.car_left = Set(Some(now_with_offset));
+
+            let car = car
+                .update(&database)
+                .await
+                .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+
+            Ok(Json(ResponseTimeLeft {
+                car_left: car.car_left.unwrap(),
+            }))
+        }
+        None => Err(StatusCode::NOT_FOUND),
+    }
+}
