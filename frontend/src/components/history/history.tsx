42c8fa853fd82717964bd6a7dc816effd3503149
kuba <kuba121201@gmail.com>
2024-05-03 17:29:58 +0200

    feat: add timeline for latest car actions

--- frontend/src/components/history/history.tsx
+++ frontend/src/components/history/history.tsx
@@ -5,50 +5,57 @@ import TimelineConnector from '@mui/lab/TimelineConnector';
 import TimelineContent from '@mui/lab/TimelineContent';
 import TimelineDot from '@mui/lab/TimelineDot';
 import TimelineOppositeContent from '@mui/lab/TimelineOppositeContent';
+import { useQuery } from '@tanstack/react-query';
+import { ICar } from '../../types/car.dto';
+
+const fetchCarData = async (): Promise<ICar[]> => {
+  const data = fetch('http://localhost:3000/cars').then((res) =>
+    res.json(),
+  )
+  return data
+}
+
+const sortCarsByLastAction = (cars: ICar[]) => {
+  cars.sort((a, b) => {
+    const dateA = a.car_left ? new Date(a.car_left) : new Date(a.car_arrived)
+    const dateB = b.car_left ? new Date(b.car_left) : new Date(b.car_arrived)
+    return dateB.getTime() - dateA.getTime();
+  });
+
+  return cars
+}
 
 export default function History() {
+  const { status, data: carData, error } = useQuery({
+    queryKey: ['cars'],
+    queryFn: fetchCarData,
+
+  })
+
+
+  if (status === 'pending') {
+    return <span>Loading...</span>
+  }
+
+  if (status === 'error') {
+    return <span>Error: {error.message}</span>
+  }
+
+
   return (
-    <Timeline position="alternate">
-      <TimelineItem>
-        <TimelineOppositeContent color="text.secondary">
-          09:30 am
-        </TimelineOppositeContent>
-        <TimelineSeparator>
-          <TimelineDot />
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent>Eat</TimelineContent>
-      </TimelineItem>
-      <TimelineItem>
-        <TimelineOppositeContent color="text.secondary">
-          10:00 am
-        </TimelineOppositeContent>
-        <TimelineSeparator>
-          <TimelineDot />
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent>Code</TimelineContent>
-      </TimelineItem>
-      <TimelineItem>
-        <TimelineOppositeContent color="text.secondary">
-          12:00 am
-        </TimelineOppositeContent>
-        <TimelineSeparator>
-          <TimelineDot />
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent>Sleep</TimelineContent>
-      </TimelineItem>
-      <TimelineItem>
-        <TimelineOppositeContent color="text.secondary">
-          9:00 am
-        </TimelineOppositeContent>
-        <TimelineSeparator>
-          <TimelineDot />
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent>Repeat</TimelineContent>
-      </TimelineItem>
-    </Timeline>
+    < Timeline position="alternate" >
+      {sortCarsByLastAction(carData).map((car, index) => (
+        <TimelineItem>
+          <TimelineOppositeContent color="text.secondary">
+            {car.car_left ? new Date(car.car_left).toLocaleString() : new Date(car.car_arrived).toLocaleString()}
+          </TimelineOppositeContent>
+          <TimelineSeparator>
+            <TimelineDot />
+            {index !== carData.length - 1 ? <TimelineConnector /> : <></>}
+          </TimelineSeparator>
+          <TimelineContent>Car {car.car_left ? 'Left' : 'Arrived'} at spot {car.spot_id}</TimelineContent>
+        </TimelineItem>
+      ))}
+    </Timeline >
   );
 }
--- frontend/src/components/slots-info/slots-info.tsx
+++ frontend/src/components/slots-info/slots-info.tsx
@@ -3,19 +3,22 @@ import Slot from '../slot/slot';
 import { useQuery } from '@tanstack/react-query';
 import { ICar } from '../../types/car.dto';
 
-const fetchCarData = async (): Promise<ICar[]> => {
+const fetchCurrentCarData = async (): Promise<ICar[]> => {
   const data = fetch('http://localhost:3000/cars?current=true').then((res) =>
     res.json(),
   )
   return data
 }
 
+const sortCarsBySpotId = (cars: ICar[]) => (
+  cars.sort((a, b) => a.spot_id - b.spot_id)
+)
 
 export default function SlotsInfoList() {
 
   const { status, data: carData, error } = useQuery({
-    queryKey: ['cars'],
-    queryFn: fetchCarData,
+    queryKey: ['current-cars'],
+    queryFn: fetchCurrentCarData,
   })
 
 
@@ -30,7 +33,7 @@ export default function SlotsInfoList() {
   return (
     <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
       {
-        carData.map(car => <Slot arriveTime={car.car_arrived} spotId={car.spot_id} key={car.id} />)
+        sortCarsBySpotId(carData).map(car => <Slot arriveTime={car.car_arrived} spotId={car.spot_id} key={car.id} />)
       }
     </List>
   );
