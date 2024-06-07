e35aada9f1fdd036a22bc7474963ea88c9b7bb0a
kuba <kuba121201@gmail.com>
2024-06-08 00:37:12 +0200

    fix cron job written in python

--- car_detection/run.py
+++ car_detection/run.py
@@ -1,10 +1,15 @@
 from crontab import CronTab
 import subprocess
+import os
+
+cron.remove_all()
 
 cron = CronTab(user=True)
 
-job = cron.new(command='./scripts/check_slots.sh')
+current_file_absolute_path = os.path.dirname(os.path.realpath(__file__))
+job = cron.new(command=f'bash {current_file_absolute_path}/scripts/check_slots.sh')
 
 job.minute.every(1)
 
 cron.write()
+
