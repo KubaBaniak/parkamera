d1eb95e0887cc354520ba72a7036ea2b541ee3eb
kuba <kuba121201@gmail.com>
2024-06-08 00:37:57 +0200

    move definition of cron above remove all

--- car_detection/run.py
+++ car_detection/run.py
@@ -2,9 +2,9 @@ from crontab import CronTab
 import subprocess
 import os
 
-cron.remove_all()
-
 cron = CronTab(user=True)
+
+cron.remove_all()
 
 current_file_absolute_path = os.path.dirname(os.path.realpath(__file__))
 job = cron.new(command=f'bash {current_file_absolute_path}/scripts/check_slots.sh')
