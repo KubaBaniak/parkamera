0df7e008b89b01d3b21ebda730e2060615d199da
kuba <kuba121201@gmail.com>
2024-06-07 23:17:33 +0200

    fix setup script

--- car_detection/scripts/setup_script.sh
+++ car_detection/scripts/setup_script.sh
@@ -4,8 +4,8 @@ rm -rf ../images/image_history/*
 rm -rf ../images/spots/*
 rm -rf ../images/base_img/*
 rm ../parking_spots_coordinates.txt
-touch parking_spots_coordinates.txt
-../../backend && sea-orm-cli migrate fresh
+touch ../parking_spots_coordinates.txt
+cd ../../backend && sea-orm-cli migrate fresh
 
 
 rpicam-still --output ../images/base_img.jpg
