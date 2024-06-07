10a356c176dc5b1fdd4f40e19aa6a72c1bc4ad0a
kuba <kuba121201@gmail.com>
2024-06-08 00:43:03 +0200

    add logs of cronjob

--- car_detection/run.py
+++ car_detection/run.py
@@ -7,7 +7,8 @@ cron = CronTab(user=True)
 cron.remove_all()
 
 current_file_absolute_path = os.path.dirname(os.path.realpath(__file__))
-job = cron.new(command=f'bash {current_file_absolute_path}/scripts/check_slots.sh')
+command = f'bash {current_file_absolute_path}/scripts/check_slots.sh > {current_file_absolute_path}/scripts/cronLogs.log 2>&1'
+job = cron.new(command=command)
 
 job.minute.every(1)
 
