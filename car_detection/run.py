0e9e71950f514a2b3abb62ab8f769357773fbc3d
kuba <kuba121201@gmail.com>
2024-06-08 00:46:07 +0200

    fix path in crontab

--- car_detection/run.py
+++ car_detection/run.py
@@ -7,7 +7,7 @@ cron = CronTab(user=True)
 cron.remove_all()
 
 current_file_absolute_path = os.path.dirname(os.path.realpath(__file__))
-command = f'bash {current_file_absolute_path}/scripts/check_slots.sh > {current_file_absolute_path}/scripts/cronLogs.log 2>&1'
+command = f'cd {current_file_absolute_path}/scripts && bash check_slots.sh > cronLogs.log 2>&1'
 job = cron.new(command=command)
 
 job.minute.every(1)
