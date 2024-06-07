4b6ec1406e36209c2987ce85963965b82f5c5e1d
kuba <kuba121201@gmail.com>
2024-06-07 22:57:50 +0200

    fix path for slots check image

--- car_detection/scripts/check_slots.sh
+++ car_detection/scripts/check_slots.sh
@@ -2,7 +2,7 @@
 
 time_stamp=$(date +%F_%H-%M-%S)
 
-rpicam-still --output "../parking_$time_stamp.jpg"
+rpicam-still --output "../images/image_history/parking_$time_stamp.jpg"
 
 . ../venv/bin/activate && python3 ../main.py
 
